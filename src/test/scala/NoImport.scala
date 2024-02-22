a79c2249ac6a3ac0d4e4fd29190190a68990130e
Adrien <adrien.crovetto@gmail.com>
2024-02-22 17:42:31 +0100

    fix compiler warning : Implicit definition should have explicit type

--- src/test/scala/NoImport.scala
+++ src/test/scala/NoImport.scala
@@ -1,3 +1,5 @@
+import play.api.libs.json.{ OFormat, Format }
+
 // test file without imports to avoid regressions with missing imports in macros
 sealed trait Modifier
 case object early extends Modifier
@@ -9,12 +11,12 @@ object a {
   import ai.x.play.json.implicits.formatSingleton
   import ai.x.play.json.{ BaseNameEncoder, NameEncoder }
   implicit val encoder: NameEncoder = BaseNameEncoder()
-  implicit def jsonFormat = ai.x.play.json.Jsonx.formatSealed[Modifier]
-  implicit def jsonFormat2 = ai.x.play.json.Jsonx.formatCaseClass[Foo]
-  implicit def jsonFormat3 = ai.x.play.json.Jsonx.formatInline[Foo]
+  implicit def jsonFormat: Format[Modifier] = ai.x.play.json.Jsonx.formatSealed[Modifier]
+  implicit def jsonFormat2: OFormat[Foo] = ai.x.play.json.Jsonx.formatCaseClass[Foo]
+  implicit def jsonFormat3: Format[Foo] = ai.x.play.json.Jsonx.formatInline[Foo]
 }
 object b {
   import ai.x.play.json.{ BaseNameEncoder, NameEncoder }
   implicit val encoder: NameEncoder = BaseNameEncoder()
-  implicit def jsonFormat4 = ai.x.play.json.Jsonx.formatAuto[Foo]
+  implicit def jsonFormat4: Format[Foo] = ai.x.play.json.Jsonx.formatAuto[Foo]
 }
